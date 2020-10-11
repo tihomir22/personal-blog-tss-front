c8cd939b171ae0026183d42864057b687e2a4cc8
Tihomir Stoychev <tihomir_alcudia3@hotmail.com>
2020-10-11 20:10:14 +0200

    changed ip

--- src/networking/Constants.tsx
+++ src/networking/Constants.tsx
@@ -1,7 +1,7 @@
 export class ConstantesNetwork {
   // public static BLOG_HOST = "http://localhost";
   //public static BLOG_HOST_PORT = "3001";
-  public static BLOG_HOST = "http://15.236.19.160";
+  public static BLOG_HOST = "http://15.188.86.76";
   public static BLOG_HOST_PORT = 49160;
   public static recoverFullBaseURL(): string {
     return this.BLOG_HOST + ":" + this.BLOG_HOST_PORT;

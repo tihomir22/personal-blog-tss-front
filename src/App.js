917a1339d1b22f23a8530101afba3b070891dee6
Tihomir Stoychev <tihomir_alcudia3@hotmail.com>
2020-12-24 14:04:18 +0100

    añadido meta-tag description

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from "react";
 import LandingPage from "./components/LadingPage.tsx";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <div>
+      <MetaTags>
+        <meta
+          name="description"
+          content="Tihomir Stoychev Stoychev. Romper lo irrompible. Evitar lo inevitable. Romper las reglas sin quebrantar la ley.
+Me defino como una persona trabajadora, apasionada, positiva y firme."
+        />
+      </MetaTags>
+      <LandingPage />
+    </div>
+  );
 }
 
 export default App;
